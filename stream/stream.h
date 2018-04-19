2f77311dcbb2a850f3e436fefbd7f914e1d1c39a
artem_fedin95 <nick.dezlol@gmail.com>
2018-04-19 15:07:05 +0700

    implement constructors

--- stream/stream.h
+++ stream/stream.h
@@ -2,48 +2,41 @@
 #define STREAM_H
 
 #include <vector>
-template<class T>
+template<class T,class Container, class Generator, class Iterator>
 class Stream{
 public:
     using size_type = std::size_t;
 public:
-    template<class Iterator>
-    Stream(Iterator begin, Iterator end){
-        for(auto it = begin; it != end; ++it){
-            data_.push_back(*it);
-        }
+
+    Stream(Iterator begin, Iterator end): area_(begin, end){}
+
+    Stream(const Container& cont): container_(cont), area_(std::begin(container_), std::end(container_)){
     }
 
-    Stream(const Container& cont): container_(cont){
+    Stream(const Container&& cont): container_(std::move(cont)), area_(std::begin(container_), std::end(container_)){}
+
+    Stream(std::initializer_list<T> init):container_(init), area_(std::begin(container_), std::end(container_)){}
+
+    Stream(Generator&& generator): generator_(generator){
     }
 
-    Stream(const Container&& cont){
-        container_ = std::move(cont);
+
+    template <typename Argument, typename... Args>
+    Stream(Argument&& first, Args&&... others)
+    {
+        container_->push_back(std::forward<Argument>(head));
+        this(std::forward<Args>(tail)...);
     }
 
-    Stream(std::initializer_list<T> init){
-        for(auto it = list.begin(); it != init.end(); ++it){
-            container_.push_back(*it);
-        }
-    }
-
-    Stream(Generator&& generator){
-        generator_ = generator;
-    }
-
-    template<class Arg, class.. Args>
-    Stream(Arg arg, Args.. rest){
-        container_.push_back(arg);
-        Stream(rest..);
-    }
 
     void get(size_type n){
 
     }
 
 private:
-    std::vector<T> data_;
     Generator generator_;
     Container container_;
+    Area<Iterator> area_;
+
 }
 #endif // STREAM_H
