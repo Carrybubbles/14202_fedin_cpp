1a512bedc0d40cd36f51ead71434eb9cab23184d
artem_fedin95 <nick.dezlol@gmail.com>
2018-05-10 10:23:23 +0700

    add FUnction class and overload operator |

--- stream/rangeview.h
+++ stream/rangeview.h
@@ -3,11 +3,43 @@
 
 #include <functional>
 #include <vector>
+#include <iostream>
 
 namespace rangeview {
 
 template <typename T, typename U>
+class RangeView;
+
+template <typename Func>
+class Function {
+public:
+    Function(Func f)
+        : fn(f)
+    {}
+
+    Function(const Function& other)
+        : fn(other.fn)
+    {}
+
+    template<typename T, typename U>
+    std::result_of_t<Func(RangeView<T, U>&&)> apply(RangeView<T, U>&& range_view) {
+        return fn(std::forward<RangeView<T, U>>(range_view));
+    }
+
+private:
+    Func fn;
+};
+
+template <typename T, typename U>
 class RangeView {
+public:
+    template <typename Func>
+    auto operator| (Function<Func> func) {
+        return func.apply(std::move(*this));
+    }
+public:
+    template<typename Func>
+    friend class Function;
 public:
 
     std::vector<U> data_ = std::vector<U>();
