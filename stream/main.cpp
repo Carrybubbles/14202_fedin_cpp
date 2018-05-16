7279f2e9a7042e0e8965ae6f03fbd96f604408e1
artem_fedin95 <nick.dezlol@gmail.com>
2018-05-16 23:02:30 +0700

    test case im main.cpp

--- stream/main.cpp
+++ stream/main.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
 #include "rangeview.h"
+#include "noisy.h"
+#include <memory>
 using namespace std;
 using namespace rangeview;
+template<typename U>
+auto kek = [](U data){
+        return std::optional<U>(std::move(data));
+    };
+
 int main()
 {
-    std::vector<int> q{15,17,19,13,14};
-    auto res_vec = (ints(1)
-                    | take(12)
-                    | transform([](auto i) {return i * 2;}) | remove_if([](auto i) {return i % 2 == 0;}) | reverse()  | take(10)).make_compose_vector();
+    std::vector<std::unique_ptr<int>> list;
+
+    for (int i = 1; i < 11; ++i)
+    {
+        list.emplace_back(std::make_unique<int>(i));
+    }
+
+//    std::unique_ptr<int> k(new int(5));
+//    using REsult =
+//    auto temp = foo(*kek<std::unique_ptr<int>>(std::move(k)));
+//    std::optional<std::unique_ptr<int>> q(std::move(temp));
+//    std::cout << *k;
+    //std::optional<std::unique_ptr<int>> opt(k);
+
+    std::vector<Noisy> q{1,2,3,4,5,6,7,8,9,10,11,12};
+    Noisy::noisy = 0;
+    auto res_vec = (q |
+                    remove_if([](auto&& i) {return i.value % 2 == 1;}) |
+                    transform([](auto&& i) {return 2*i.value;}) |
+                    reverse()
+                    | take(20)
+                    );
+//    std::cout << res_vec.size();
+//    for(auto a: res_vec){
+//        std::cout << a << std::endl;
+//    }
+
     return 0;
 }
